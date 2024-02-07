b64a9885388eb261e9f59ad9fbd6abca33a311a6
Sofia Håbrekke <sofiash@stud.ntnu.no>
2024-02-07 13:14:55 +0100

    Feat: Create add method

--- src/main/java/edu/ntnu/idatt2003/group25/Vector2D.java
+++ src/main/java/edu/ntnu/idatt2003/group25/Vector2D.java
@@ -1,5 +1,7 @@
 package edu.ntnu.idatt2003.group25;
 
+import java.util.Vector;
+
 public class Vector2D {
   private double x0;
   private double x1;
@@ -14,5 +16,12 @@ public class Vector2D {
   }
   public double getX1(){
     return x1;
+  }
+
+  public Vector2D add(Vector2D other){
+    double newX0 = x0 + other.getX0();
+    double newX1 = x1 + other.getX1();
+
+    return new Vector2D(newX0, newX1);
   }
 }
