5624ca22111134d140362870f00c3ce28502d048
Johanne Fixdal <johanfix@stud.ntnu.no>
2024-02-07 13:19:58 +0100

    Feat: Init Matrix2x2 constructor

--- src/main/java/edu/ntnu/idatt2003/group25/Matrix2x2.java
+++ src/main/java/edu/ntnu/idatt2003/group25/Matrix2x2.java
@@ -2,4 +2,13 @@ package edu.ntnu.idatt2003.group25;
 
 public class Matrix2x2 {
 
+  double a00;
+  double a01;
+  double a10;
+  double a11;
+
+  public Matrix2x2(double a00,double a01, double a10, double a11) {
+
+  }
+
 }
