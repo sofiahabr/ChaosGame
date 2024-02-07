d45b46636175e2921ac4d8b476bd285efd8da12f
Sofia Håbrekke <sofiash@stud.ntnu.no>
2024-02-07 13:08:26 +0100

    Feat: Create Vector2D class

---
+++ src/main/java/edu/ntnu/idatt2003/group25/Vector2D.java
@@ -0,0 +1,18 @@
+package edu.ntnu.idatt2003.group25;
+
+public class Vector2D {
+  private double x0;
+  private double x1;
+
+  public Vector2D(double x0, double x1){
+    this.x0 = x0;
+    this.x1 = x1;
+  }
+
+  public double getX0(){
+    return x0;
+  }
+  public double getX1(){
+    return x1;
+  }
+}
