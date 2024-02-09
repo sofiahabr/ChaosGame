1d0a91ece18235cf28f9c5e5104bb3a8714de7ab
Sofia Håbrekke <sofiash@stud.ntnu.no>
2024-02-09 13:54:04 +0100

    Feat: Create method for sqrt

--- src/main/java/edu/ntnu/idatt2003/group25/Complex.java
+++ src/main/java/edu/ntnu/idatt2003/group25/Complex.java
@@ -12,8 +12,15 @@ public class Complex extends Vector2D {
     super(realPart, imaginaryPart);
   }
 
-  public Complex sqrt(double realPart, double imaginaryPart){
-    
+  public Complex sqrt(){
+    double absZ = Math.sqrt(Math.pow(getX0(), 2) + Math.pow(getX1(), 2));
+    double seperatedReal = getX0() + absZ;
+    double absDenominator = Math.sqrt(Math.pow(seperatedReal, 2) + Math.pow(getX1(), 2));
+
+    double newRealPart = Math.sqrt(absZ)*seperatedReal / absDenominator;
+    double newImaginaryPart = Math.sqrt(absZ)*getX1() / absDenominator;
+
+    return new Complex(newRealPart, newImaginaryPart);
   }
 
 
