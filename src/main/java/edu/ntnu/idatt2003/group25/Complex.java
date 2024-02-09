d9457977ae55bb19cf590b1b52a69e11ac0a3324
Johanne Fixdal <johanfix@stud.ntnu.no>
2024-02-09 12:29:52 +0100

    Feat: create Comlex class

---
+++ src/main/java/edu/ntnu/idatt2003/group25/Complex.java
@@ -0,0 +1,20 @@
+package edu.ntnu.idatt2003.group25;
+
+public class Complex extends Vector2D {
+
+  /**
+   *
+   */
+
+  double realPart;
+  double imaginaryPart;
+  public Complex(double realPart, double imaginaryPart) {
+    super(realPart, imaginaryPart);
+  }
+
+  public Complex sqrt(double realPart, double imaginaryPart){
+    
+  }
+
+
+}
