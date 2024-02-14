f480896070cc0ddbb203491c7e0e561561bd1a84
Sofia Håbrekke <sofiash@stud.ntnu.no>
2024-02-14 15:01:02 +0100

    Feat: Create class JuliaTransform and method for transformation

---
+++ src/main/java/edu/ntnu/idatt2003/group25/JuliaTransform.java
@@ -0,0 +1,16 @@
+package edu.ntnu.idatt2003.group25;
+
+public class JuliaTransform implements Transform2D{
+  private final Complex point;
+  private final int sign;
+
+  public JuliaTransform(Complex point, int sign){
+    this.point = point;
+    this.sign = sign;
+  }
+  @Override
+  public Vector2D transform(Vector2D otherPoint) {
+    Complex newComplex = point.subtract(otherPoint);
+    return newComplex.sqrt().scale(sign);
+  }
+}
