c87ae4c455cdb8f35e9392984f2786650bd1a6eb
Sofia Håbrekke <sofiash@stud.ntnu.no>
2024-02-14 13:25:58 +0100

    Refactor: Change implementation of sqrt method

--- src/main/java/edu/ntnu/idatt2003/group25/Complex.java
+++ src/main/java/edu/ntnu/idatt2003/group25/Complex.java
@@ -13,12 +13,8 @@ public class Complex extends Vector2D {
   }
 
   public Complex sqrt(){
-    double absZ = Math.sqrt(Math.pow(getX0(), 2) + Math.pow(getX1(), 2));
-    double seperatedReal = getX0() + absZ;
-    double absDenominator = Math.sqrt(Math.pow(seperatedReal, 2) + Math.pow(getX1(), 2));
-
-    double newRealPart = Math.sqrt(absZ)*seperatedReal / absDenominator;
-    double newImaginaryPart = Math.sqrt(absZ)*getX1() / absDenominator;
+    double newRealPart = Math.sqrt(0.5*(Math.sqrt(Math.pow(getX0(),2) + Math.pow(getX1(),2)) + getX0()));
+    double newImaginaryPart = Math.signum(getX1())*Math.sqrt(0.5*(Math.sqrt(Math.pow(getX0(), 2) + Math.pow(getX1(), 2)) - getX0()));
 
     return new Complex(newRealPart, newImaginaryPart);
   }
