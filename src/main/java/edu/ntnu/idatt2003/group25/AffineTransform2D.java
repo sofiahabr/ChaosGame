475c88470526ce02b86f4a47b6ea621528596039
Sofia Håbrekke <sofiash@stud.ntnu.no>
2024-03-06 13:32:19 +0100

    Docs: Generate javadocs

--- src/main/java/edu/ntnu/idatt2003/group25/AffineTransform2D.java
+++ src/main/java/edu/ntnu/idatt2003/group25/AffineTransform2D.java
@@ -1,14 +1,36 @@
 package edu.ntnu.idatt2003.group25;
 
+/**
+ * The type Affine transform 2 d.
+ */
 public class AffineTransform2D implements Transform2D {
 
+  /**
+   * The Matrix.
+   */
   Matrix2x2 matrix;
+  /**
+   * The Vector.
+   */
   Vector2D vector;
 
+  /**
+   * Instantiates a new Affine transform 2 d.
+   *
+   * @param matrix the matrix
+   * @param vector the vector
+   */
   public AffineTransform2D(Matrix2x2 matrix, Vector2D vector) {
     this.matrix = matrix;
     this.vector = vector;
   }
+
+  /**
+   * Affine transforms a vector.
+   *
+   * @param point is a Vector
+   * @return the transformed vector.
+   */
   @Override
   public Vector2D transform(Vector2D point) {
     return matrix.multiply(point).add(vector);
