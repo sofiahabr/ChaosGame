0284ceacb6559c0016fc35f719877d624d616b39
Johanne Fixdal <johanfix@stud.ntnu.no>
2024-03-20 13:36:30 +0100

    Fix: write javaDoc

--- src/main/java/edu/ntnu/idatt2003/group25/Complex.java
+++ src/main/java/edu/ntnu/idatt2003/group25/Complex.java
@@ -8,30 +8,19 @@ public class Complex extends Vector2D {
   /**
    * The Real part.
    */
+
   double realPart;
   /**
    * The Imaginary part.
    */
   double imaginaryPart;
-
-  /**
-   * Instantiates a new Complex.
-   *
-   * @param realPart      the real part
-   * @param imaginaryPart the imaginary part
-   */
   public Complex(double realPart, double imaginaryPart) {
     super(realPart, imaginaryPart);
   }
 
-  /**
-   * Calculates the square root of a complex number.
-   *
-   * @return the square root.
-   */
   public Complex sqrt(){
     double newRealPart = Math.sqrt(0.5*(Math.sqrt(Math.pow(getX0(),2) + Math.pow(getX1(),2)) + getX0()));
-    double newImaginaryPart = Math.signum(getX1())*Math.sqrt(0.5 * (Math.sqrt(Math.pow(getX0(), 2) + Math.pow(getX1(), 2)) - getX0()));
+    double newImaginaryPart = Math.signum(getX1())*Math.sqrt(0.5*(Math.sqrt(Math.pow(getX0(), 2) + Math.pow(getX1(), 2)) - getX0()));
 
     return new Complex(newRealPart, newImaginaryPart);
   }
