9f487c42e107f6f1426107ad38d9c6b4894ea63d
Johanne Fixdal <johanfix@stud.ntnu.no>
2024-03-20 13:57:41 +0100

    Fix: bug with reading comments

--- src/main/java/edu/ntnu/idatt2003/group25/ChaosGameFileHandler.java
+++ src/main/java/edu/ntnu/idatt2003/group25/ChaosGameFileHandler.java
@@ -23,13 +23,13 @@ public class ChaosGameFileHandler extends ChaosGameDescription{
       String firstLine = scanner.nextLine();
       String transformName = firstLine.split("#")[0];
 
-      String[] minimumValues = scanner.nextLine().split(",");
+      String[] minimumValues = scanner.nextLine().split("(,)|(#)");
       double min0 = Double.parseDouble(minimumValues[0]);
       double min1 = Double.parseDouble(minimumValues[1]);
 
       Vector2D min = new Vector2D(min0, min1);
 
-      String[] maximumValues = scanner.nextLine().split(",");
+      String[] maximumValues = scanner.nextLine().split("(,)|(#)");
       double max0 = Double.parseDouble(maximumValues[0]);
       double max1 = Double.parseDouble(maximumValues[1]);
 
@@ -38,7 +38,7 @@ public class ChaosGameFileHandler extends ChaosGameDescription{
       List<Transform2D> transforms = new ArrayList<>();
       switch (transformName) {
         case ("Julia"):
-          String[] c = scanner.nextLine().split(",");
+          String[] c = scanner.nextLine().split("(,)|(#)");
           double x0 = Double.parseDouble(c[0]);
           double x1 = Double.parseDouble(c[1]);
 
@@ -49,7 +49,7 @@ public class ChaosGameFileHandler extends ChaosGameDescription{
           break;
         case ("Affine2D"):
           while (scanner.hasNext()) {
-            String[] line = scanner.nextLine().split(",");
+            String[] line = scanner.nextLine().split("(,)|(#)");
             double a00 = Double.parseDouble(line[0]);
             double a01 = Double.parseDouble(line[1]);
             double a10 = Double.parseDouble(line[2]);
@@ -72,14 +72,12 @@ public class ChaosGameFileHandler extends ChaosGameDescription{
     } catch (FileNotFoundException e) {
       System.out.println("Error: " + e.getMessage());
     }
-      //leser en og en linje fra fil ved static scanner i egen klasse
-      // sjekker første linje -> Julia (behandler info om Julia) -> Affine (behandler affine)
-      // finner max og min, samt setter alle transformasjonen inn i transform ArrayList<>
-      //return new ChaosGameDescription(Transform2D,max, min);
     return null;
     }
 
-  public void writeToFile(ChaosGameDescription description, String path) {}
+  public void writeToFile(ChaosGameDescription description, String path) {
+
+  }
 }
 
 
