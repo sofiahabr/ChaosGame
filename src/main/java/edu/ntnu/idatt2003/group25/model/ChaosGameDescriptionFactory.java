ef84aca3c4cf9e1ebb3866e9cca1d70c59d96253
Johanne Fixdal <johanfix@stud.ntnu.no>
2024-04-17 13:37:01 +0200

    Fix: removed duplicate transform

--- src/main/java/edu/ntnu/idatt2003/group25/model/ChaosGameDescriptionFactory.java
+++ src/main/java/edu/ntnu/idatt2003/group25/model/ChaosGameDescriptionFactory.java
@@ -2,11 +2,10 @@ package edu.ntnu.idatt2003.group25.model;
 
 import java.util.ArrayList;
 import java.util.List;
-import javax.sound.midi.Soundbank;
 
 /**
  * The ChaosGameDescriptionFactory creates ChaosGameDescriptions for four different fractals.
- * The factory creates descriptions for Sierpinski-, Barnsley-Fern-, Affine and Julia fractals.
+ * The factory creates descriptions for Sierpinski-, Barnsley-Fern- and Julia fractals.
  */
 public class ChaosGameDescriptionFactory {
 
@@ -35,18 +34,6 @@ public class ChaosGameDescriptionFactory {
         new Vector2D(-2.64, 0), new Vector2D(2.65, 10));
   }
 
-  static ChaosGameDescription createAffineTransformation() {
-    List<Transform2D> transformation = new ArrayList<>();
-    transformation.add(new AffineTransform2D(new Matrix2x2(0.5, 0, 0, 0.5),
-        new Vector2D(0, 0)));
-    transformation.add(new AffineTransform2D(new Matrix2x2(0.5, 0, 0, 0.5),
-        new Vector2D(0.25, 0.5)));
-    transformation.add(new AffineTransform2D(new Matrix2x2(0.5, 0, 0, 0.5),
-        new Vector2D(0.5, 0)));
-    return new ChaosGameDescription(transformation, new Vector2D(0, 0), new Vector2D(1, 1));
-  }
-
-  //TODO: Consider if Julia should have 1 or 2 transformations
   static ChaosGameDescription createJuliaTransformation(Complex c) {
     List<Transform2D> transformation = new ArrayList<>();
     transformation.add(new JuliaTransform(c, 1));
