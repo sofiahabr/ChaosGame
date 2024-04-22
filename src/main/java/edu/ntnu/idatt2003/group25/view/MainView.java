84fdaa7f87f5de5f1c96f1ef3e5b857930b6efb5
Sofia Håbrekke <sofiash@stud.ntnu.no>
2024-04-22 10:43:53 +0200

    Refactor: Implement Observer

--- src/main/java/edu/ntnu/idatt2003/group25/view/MainView.java
+++ src/main/java/edu/ntnu/idatt2003/group25/view/MainView.java
@@ -1,30 +1,41 @@
 package edu.ntnu.idatt2003.group25.view;
 
-import edu.ntnu.idatt2003.group25.model.ChaosCanvas;
 import edu.ntnu.idatt2003.group25.model.ChaosGame;
-import edu.ntnu.idatt2003.group25.model.ChaosGameDescription;
-import edu.ntnu.idatt2003.group25.model.ChaosGameFileHandler;
-import edu.ntnu.idatt2003.group25.model.Vector2D;
-import java.util.ArrayList;
+import edu.ntnu.idatt2003.group25.model.ChaosGameDescriptionFactory;
+import edu.ntnu.idatt2003.group25.model.ChaosGameObserver;
+import edu.ntnu.idatt2003.group25.model.Complex;
+import java.util.Observer;
 import javafx.application.Application;
+import javafx.beans.Observable;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-public class MainView extends Application {
+public class MainView extends Application implements ChaosGameObserver {
   static int height = 900;
   static int width = 1200;
+  BorderPane mainPage = new BorderPane();
+  FactorialPage factorialPage = new FactorialPage();
 
   @Override
-  public void start(Stage stage) throws Exception {
-    FactorialPage factorialPage = new FactorialPage();
-    InitializePage initializePage = new InitializePage();
+  public void start(Stage stage)  {
+//    InitializePage initializePage = new InitializePage();
 
-
-    BorderPane mainPage = new BorderPane();
+    HBox topBar = new HBox();
+    topBar.setPadding(new Insets(40));
+    topBar.setStyle("-fx-background-color: #708090");
 
     mainPage.setCenter(factorialPage.factorialPane());
+    mainPage.setTop(topBar);
     Scene scene = new Scene(mainPage, width, height);
 
 
@@ -37,4 +48,10 @@ public class MainView extends Application {
   public static void main(String[] args) {
     launch(args);
   }
+
+  @Override
+  public void gameChanged() {
+
+  }
 }
+
