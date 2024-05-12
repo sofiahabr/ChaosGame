474538a56249c1c970844465f4cc91e10f96ab27
Sofia Håbrekke <sofiash@stud.ntnu.no>
2024-05-12 16:18:31 +0200

    Fix: Remove ChaosGame as a subject

--- src/main/java/edu/ntnu/idatt2003/group25/model/ChaosGame.java
+++ src/main/java/edu/ntnu/idatt2003/group25/model/ChaosGame.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public class ChaosGame implements ChaosGameSubject{
+public class ChaosGame{
   private ChaosCanvas canvas;
   private ChaosGameDescription description;
   private Vector2D currentPoint;  // punktet den ved en hver tid befinner seg
@@ -31,25 +31,7 @@ public class ChaosGame implements ChaosGameSubject{
       canvas.putPixel(newPoint);
       this.currentPoint = newPoint;
 
-      updateObserver();
 
-    }
-  }
-
-  @Override
-  public void addObserver(ChaosGameObserver observer) {
-    observerList.add(observer);
-  }
-
-  @Override
-  public void removeObserver(ChaosGameObserver observer) {
-    observerList.remove(observer);
-  }
-
-  @Override
-  public void updateObserver() {
-    for (ChaosGameObserver observer : observerList) {
-      observer.gameChanged();
     }
   }
 }
