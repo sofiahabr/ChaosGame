b03442b904e33ab4068354650524bd0d9d3a3c2e
Sofia Håbrekke <sofiash@stud.ntnu.no>
2024-05-12 16:19:11 +0200

    Fix: split ChaosGameObserver and ChaosGameSubject into two classes

--- src/main/java/edu/ntnu/idatt2003/group25/model/ChaosGameObserver.java
+++ src/main/java/edu/ntnu/idatt2003/group25/model/ChaosGameObserver.java
@@ -1,23 +1,17 @@
 package edu.ntnu.idatt2003.group25.model;
 
 public interface ChaosGameObserver {
-  void gameChanged();
-}
-interface ChaosGameSubject {
-  void addObserver(ChaosGameObserver observer);
-  void removeObserver(ChaosGameObserver observer);
-  void updateObserver();
+  void gameChanged(String string);
 }
 
-/**
- * Temporary observer who will be used in GUI for updating ChaosGame
- */
-//TODO add functionality for the observers
-class ObserverObject implements ChaosGameObserver {
-  @Override
-  public void gameChanged() {
-    System.out.println("Update observer");
-  }
+///**
+// * Temporary observer who will be used in GUI for updating ChaosGame
+// */
+////TODO add functionality for the observers
+//class ObserverObject implements ChaosGameObserver {
+//  @Override
+//  public void gameChanged() {
+//    System.out.println("Update observer");
+//  }
 
-}
 
