d792ed5a24725b75c7a9fa3116132beb226b809e
Sofia Håbrekke <sofiash@stud.ntnu.no>
2024-05-12 15:16:38 +0200

    Fix: Set InitPage as center in MainPage pane

--- src/main/java/edu/ntnu/idatt2003/group25/view/MainView.java
+++ src/main/java/edu/ntnu/idatt2003/group25/view/MainView.java
@@ -1,23 +1,10 @@
 package edu.ntnu.idatt2003.group25.view;
-
-import edu.ntnu.idatt2003.group25.model.ChaosGame;
-import edu.ntnu.idatt2003.group25.model.ChaosGameDescriptionFactory;
 import edu.ntnu.idatt2003.group25.model.ChaosGameObserver;
-import edu.ntnu.idatt2003.group25.model.Complex;
-import java.util.Observer;
 import javafx.application.Application;
-import javafx.beans.Observable;
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class MainView extends Application implements ChaosGameObserver {
@@ -28,13 +15,13 @@ public class MainView extends Application implements ChaosGameObserver {
 
   @Override
   public void start(Stage stage)  {
-//    InitializePage initializePage = new InitializePage();
+    InitializePage initializePage = new InitializePage();
 
     HBox topBar = new HBox();
     topBar.setPadding(new Insets(40));
     topBar.setStyle("-fx-background-color: #708090");
 
-    mainPage.setCenter(factorialPage.factorialPane());
+    mainPage.setCenter(initializePage.getInitPane());
     mainPage.setTop(topBar);
     Scene scene = new Scene(mainPage, width, height);
 
