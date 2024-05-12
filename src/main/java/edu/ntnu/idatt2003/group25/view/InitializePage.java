9b62e964f01e53a0d31fc103f19df7c833418441
Sofia Håbrekke <sofiash@stud.ntnu.no>
2024-05-12 16:23:58 +0200

    Feat: Make InitPage extend View superclass

--- src/main/java/edu/ntnu/idatt2003/group25/view/InitializePage.java
+++ src/main/java/edu/ntnu/idatt2003/group25/view/InitializePage.java
@@ -5,23 +5,26 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 
-public class InitializePage {
+public class InitializePage extends View {
   private BorderPane initPane = new BorderPane();
 
   public InitializePage(){
-    initPane.setStyle("-fx-background-color: white; ");
-    chooseTransformTypeBox();
+    setUp();
   }
-  public void chooseTransformTypeBox() {
+
+  @Override
+  public void setUp() {
+    initPane.setStyle("-fx-background-color: white; ");
+
     StackPane infoBoxBackground = new StackPane();
     Rectangle box = new Rectangle(600, 400);
-    box.setStyle("-fx-fill: #708090; -fx-border-width:20; -fx-border-radius: 100;");
+    box.setStyle("-fx-fill: #D9D9D9; -fx-border-width: 20; -fx-border-color: black; -fx-border-radius: 100;");
 
     Text heading = new Text("Chaos Game");
     heading.setStyle("-fx-font-size: 30");
@@ -35,6 +38,7 @@ public class InitializePage {
 
     optionBox.setMinSize(180, 30);
     optionBox.setStyle("-fx-background-color: white");
+    optionBox.setOnAction(e-> {updateObserver(optionBox.getValue());});
 
     Button startButton  = new Button("Start Game");
     startButton.setStyle("-fx-font-size: 16; -fx-background-color: white; -fx-padding: 5 20; -fx-border-radius: 10");
@@ -50,7 +54,9 @@ public class InitializePage {
     initPane.setCenter(infoBoxBackground);
   }
 
-  public BorderPane getInitPane() {
+  @Override
+  public Pane getPane() {
     return initPane;
   }
+
 }
