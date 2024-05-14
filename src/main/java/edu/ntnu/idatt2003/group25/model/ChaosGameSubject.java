98ab6548cca0e038cf930453bd2ccd21d739abf8
Sofia Håbrekke <sofiash@stud.ntnu.no>
2024-05-14 10:16:19 +0200

    Refactor: Make "update observer" have 2 attributes

--- src/main/java/edu/ntnu/idatt2003/group25/model/ChaosGameSubject.java
+++ src/main/java/edu/ntnu/idatt2003/group25/model/ChaosGameSubject.java
@@ -3,5 +3,5 @@ package edu.ntnu.idatt2003.group25.model;
 public interface ChaosGameSubject {
   void addObserver(ChaosGameObserver observer);
   void removeObserver(ChaosGameObserver observer);
-  void updateObserver(String string);
+  void updateObserver(String event, String info);
 }
