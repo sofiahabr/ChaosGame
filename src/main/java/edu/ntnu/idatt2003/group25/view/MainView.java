f1f8206ec2815e270f117129294a16c029f2dfc6
Johanne Fixdal <johanfix@stud.ntnu.no>
2024-05-15 13:05:36 +0200

    Refactor: separated mainView and mainLogic in two different classes

--- src/main/java/edu/ntnu/idatt2003/group25/view/MainView.java
+++ src/main/java/edu/ntnu/idatt2003/group25/view/MainView.java
@@ -11,32 +11,15 @@ import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class MainView extends Application  {
-  public static int height = 900;
-  public static int width = 1200;
-  public static BorderPane mainPage = new BorderPane();
-  public static ChaosGameDescription description = ChaosGameDescriptionFactory.createBarnsleyFern();
 
-
-  public Scene scene = new Scene(new Pane(), width, height);
-  public ScreenController screenController = new ScreenController(scene);
-  public InitializePage initializePage = new InitializePage(screenController);
-  public FactorialPage factorialPage = new FactorialPage(screenController);
-
+private final MainLogic mainLogic = new MainLogic();
 
   @Override
   public void start(Stage stage)  {
-
-    screenController.addScreenContent(factorialPage, "factorial page");
-    screenController.addScreenContent(initializePage, "initialize page");
-
-    screenController.gameChanged( "switch page","initialize page");
-
-    stage.setTitle("Chaos Game");
-    stage.setScene(scene);
-    stage.show();
-
+    mainLogic.start(stage);
 
   }
+
   public static void main(String[] args) {
     launch(args);
   }
