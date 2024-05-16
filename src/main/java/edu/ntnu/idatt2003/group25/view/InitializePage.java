199563d24728a76e005459e6dae22623089bbe02
Johanne Fixdal <johanfix@stud.ntnu.no>
2024-05-16 10:32:19 +0200

    Fix: removed comments

--- src/main/java/edu/ntnu/idatt2003/group25/view/InitializePage.java
+++ src/main/java/edu/ntnu/idatt2003/group25/view/InitializePage.java
@@ -1,19 +1,13 @@
 package edu.ntnu.idatt2003.group25.view;
 
-import edu.ntnu.idatt2003.group25.controller.Controller;
+
 import edu.ntnu.idatt2003.group25.controller.InitializePageController;
 import edu.ntnu.idatt2003.group25.controller.ScreenController;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Node;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.TreeItem;
-import javafx.scene.control.TreeView;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.shape.Rectangle;
@@ -21,7 +15,7 @@ import javafx.scene.text.Text;
 
 public class InitializePage extends View {
   ScreenController screenController;
-  private BorderPane initPane = new BorderPane();
+  private final BorderPane initPane = new BorderPane();
 
   public InitializePage(ScreenController screenController){
     this.screenController = screenController;
@@ -36,7 +30,6 @@ public class InitializePage extends View {
     StackPane infoBoxBackground = new StackPane();
     Rectangle box = new Rectangle(600, 400);
     box.getStyleClass().add("rectangle");
-//    box.setStyle("-fx-fill: #D9D9D9; -fx-border-width: 20; -fx-border-color: black; -fx-border-radius: 100;");
 
     Text heading = new Text("Chaos Game");
     heading.getStyleClass().add("heading");
@@ -52,11 +45,8 @@ public class InitializePage extends View {
         "Sierpinski Triangle", "Barnsley Fern", "Read from file");
 
     optionBox.setMinSize(180, 30);
-//    optionBox.setStyle("-fx-background-color: white");
 
     Button startButton  = new Button("Start Game");
-//    startButton.setStyle("-fx-font-size: 16; -fx-background-color: white; -fx-padding: 5 20; -fx-border-radius: 10");
-
     startButton.setOnAction(e-> {
       if (optionBox.getValue() == null) {
         optionBox.setPromptText("Please choose a transform");
