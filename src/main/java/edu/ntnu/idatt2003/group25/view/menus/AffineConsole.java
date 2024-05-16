3f35cc434ed270a87aaa8f589ce34817a41531a0
Sofia Håbrekke <sofiash@stud.ntnu.no>
2024-05-16 16:23:07 +0200

    Refactor: Add FactorialPage as attribute in consoles

--- src/main/java/edu/ntnu/idatt2003/group25/view/menus/AffineConsole.java
+++ src/main/java/edu/ntnu/idatt2003/group25/view/menus/AffineConsole.java
@@ -1,10 +1,11 @@
 package edu.ntnu.idatt2003.group25.view.menus;
 
 import edu.ntnu.idatt2003.group25.controller.ScreenController;
+import edu.ntnu.idatt2003.group25.view.FactorialPage;
 
 public class AffineConsole extends SideMenu {
-  public AffineConsole(ScreenController screenController) {
-    super(screenController);
+  public AffineConsole(ScreenController screenController, FactorialPage factorialPage) {
+    super(screenController, factorialPage);
   }
   @Override
   public void setUp() {
