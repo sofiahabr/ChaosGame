96249de96e103bd9aaf7be0c3ab4bc5cb83c88c3
Sofia Håbrekke <sofiash@stud.ntnu.no>
2024-05-18 11:47:21 +0200

    Fix: update stylesheet location

--- src/main/java/edu/ntnu/idatt2003/group25/view/MainView.java
+++ src/main/java/edu/ntnu/idatt2003/group25/view/MainView.java
@@ -24,7 +24,7 @@ public class MainView extends Application  {
     screenController.addScreenContent(initializePage, "initialize page");
 
     screenController.gameChanged( "switch page","initialize page");
-    scene.getStylesheets().add("/style.css");
+    scene.getStylesheets().add("/style/style.css");
 
     stage.setTitle("Chaos Game");
     stage.setScene(scene);
