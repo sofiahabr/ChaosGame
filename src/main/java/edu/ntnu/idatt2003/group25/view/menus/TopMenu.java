ae36d451a6f199197357ae866621b5fbbdc122a3
Sofia Håbrekke <sofiash@stud.ntnu.no>
2024-05-18 18:50:17 +0200

    Style: rename and relabel buttons

--- src/main/java/edu/ntnu/idatt2003/group25/view/menus/TopMenu.java
+++ src/main/java/edu/ntnu/idatt2003/group25/view/menus/TopMenu.java
@@ -19,17 +19,22 @@ public class TopMenu extends Menu {
   public void setUp() {
     menu.getStyleClass().add("hbox");
 
-    Button backButton = new Button("Back");
-    backButton.setOnAction(e -> {
-      updateObserver("back", "initialize page");
-    });
-    backButton.setAlignment(Pos.CENTER);
+    Button homeButton = new Button("Home");
 
-    Button exitButton = new Button("Exit");
+    homeButton.getStyleClass().add("heading");
+    homeButton.setStyle("-fx-font: 30px");
+
+    homeButton.setOnAction(e -> {
+      updateObserver("home", "initialize page");
+    });
+    homeButton.setAlignment(Pos.CENTER);
+
+    Button exitButton = new Button("X");
     exitButton.setOnAction(e -> Platform.exit());
     exitButton.setAlignment(Pos.CENTER_RIGHT);
 
-    Button infoButton = new Button("i");
+    Button infoButton = new Button("Help and instructions");
+
 
     //TODO: drop down informasjon om spillet og hvordan det fungerer
     // TODO: Ikoner for back, exit og info + spillnavn i midten
@@ -39,7 +44,7 @@ public class TopMenu extends Menu {
 
     menu.setPrefHeight(60);
     menu.setPadding(new Insets(20));
-    menu.getChildren().addAll(backButton, buffer,infoButton, exitButton);
+    menu.getChildren().addAll(homeButton, buffer,infoButton, exitButton);
   }
 
   @Override
