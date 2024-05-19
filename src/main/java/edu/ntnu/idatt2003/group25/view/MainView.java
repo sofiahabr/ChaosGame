2895e9685f185d9602155d7369d9bc10c64eaa22
Johanne Fixdal <johanfix@stud.ntnu.no>
2024-05-19 15:13:09 +0200

    Fix: removed unused imports

--- src/main/java/edu/ntnu/idatt2003/group25/view/MainView.java
+++ src/main/java/edu/ntnu/idatt2003/group25/view/MainView.java
@@ -1,13 +1,5 @@
 package edu.ntnu.idatt2003.group25.view;
-import edu.ntnu.idatt2003.group25.controller.ScreenController;
-import edu.ntnu.idatt2003.group25.model.ChaosGameDescription;
-import edu.ntnu.idatt2003.group25.model.ChaosGameDescriptionFactory;
 import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class MainView extends Application {
