b80710c61f54d984e3416de02f50119f239a6bde
Johanne Fixdal <johanfix@stud.ntnu.no>
2024-05-19 16:43:45 +0200

    Feat: implement new addResizeListener method for updating about screen size changes

--- src/main/java/edu/ntnu/idatt2003/group25/view/FactorialPage.java
+++ src/main/java/edu/ntnu/idatt2003/group25/view/FactorialPage.java
@@ -6,6 +6,7 @@ import edu.ntnu.idatt2003.group25.model.ChaosGame;
 import edu.ntnu.idatt2003.group25.model.ChaosGameDescription;
 import edu.ntnu.idatt2003.group25.model.ChaosGameDescriptionFactory;
 import edu.ntnu.idatt2003.group25.view.menus.Menu;
+import edu.ntnu.idatt2003.group25.view.menus.TopMenu;
 import java.util.HashMap;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -30,8 +31,7 @@ public class FactorialPage extends View {
   private String gameType; // Julia or Affine
   private ChaosGame chaosGame;
   private Canvas pixelCanvas;
-
-  HashMap<String,String> errorMap = new HashMap<>();
+  HashMap<String, String> errorMap = new HashMap<>();
   private Label stepsErrorLabel;
   private Label minMaxError;
   private Label matrixErrorLabel;
@@ -42,23 +42,36 @@ public class FactorialPage extends View {
     this.screenController = screenController;
     addObserver(controller);
     initilizeCanvas();
+    bindCanvasSize();
+    addResizeListener();
   }
+
   private void initilizeCanvas() {
     this.pixelCanvas = new Canvas();
-    this.chaosGame = new ChaosGame(description, (int) (MainLogic.width*0.7),
+    this.chaosGame = new ChaosGame(description, (int) (MainLogic.width * 0.7),
         (int) (MainLogic.height * 0.7));
-    bindCanvasSize();
   }
+
   private void bindCanvasSize() {
     pixelCanvas.widthProperty().bind(borderPane.widthProperty().multiply(0.7));
     pixelCanvas.heightProperty().bind(borderPane.heightProperty().multiply(0.7));
   }
+  private void addResizeListener() {
+    borderPane.widthProperty().addListener((obs, oldValue, newValue) -> {
+      controller.handleScreenResize(newValue.doubleValue(), borderPane.getHeight());
+    });
+    borderPane.heightProperty().addListener((obs, oldValue, newValue) -> {
+      controller.handleScreenResize(borderPane.getWidth(), newValue.doubleValue());
+    });
+  }
+
   @Override
   public void setUp() {
-    HBox topBox = new HBox(0);
-    borderPane.setTop(topBox);
 
-    Menu console;
+    TopMenu topMenu = new TopMenu(screenController);
+    topMenu.setUp();
+    borderPane.setTop(topMenu.getMenu());
+
     if (gameType.equals("Julia Transform")) {
       juliaSidebar();
     } else {
@@ -72,11 +85,13 @@ public class FactorialPage extends View {
   @Override
   public void update() {
     this.description = MainLogic.description;
-    initilizeCanvas();
+    chaosGame = new ChaosGame(description, Math.round(MainLogic.width * 0.7f),
+        Math.round(MainLogic.height * 0.7f));
+//    initilizeCanvas();
   }
 
   private void createSideBar() {
-    sidebarMenu = new VBox(40);
+    sidebarMenu = new VBox(10);
     sidebarMenu.getStyleClass().add("vbox");
     sidebarMenu.setPadding(new Insets(20));
   }
@@ -84,7 +99,6 @@ public class FactorialPage extends View {
   public VBox chooseStepsField() {
     Text chooseSteps = new Text("Choose steps");
     chooseSteps.getStyleClass().add("heading");
-
 
     TextField inputSteps = new TextField();
     inputFieldStyle(inputSteps, "Ex. 500...", 30, 180);
@@ -99,12 +113,13 @@ public class FactorialPage extends View {
     stepsArea.setAlignment(Pos.CENTER);
 
     // send to controller
-    inputSteps.setOnKeyTyped(e-> {
-        updateObserver("register steps", inputSteps.getText());
+    inputSteps.setOnKeyTyped(e -> {
+      updateObserver("register steps", inputSteps.getText());
     });
 
     return stepsArea;
   }
+
   private VBox chooseMinMaxField() {
 
     // Create min/ max area
@@ -114,16 +129,32 @@ public class FactorialPage extends View {
     minTitle.getStyleClass().add("heading");
 
     TextField inputMax = new TextField();
-    inputFieldStyle(inputMax,"ex. 1", 30, 80);
-    inputMax.setOnKeyTyped(e-> updateObserver("max input", inputMax.getText() + ", "
-        + inputMax.getText()));
+    inputFieldStyle(inputMax, "1", 30, 35);
 
+    TextField inputMax2 = new TextField();
+    inputFieldStyle(inputMax2, "1", 30, 35);
+
+    HBox maxArea = new HBox(10);
+    maxArea.getChildren().addAll(inputMax, inputMax2);
+
+    inputMax.setOnKeyTyped(
+        e -> updateObserver("max input", inputMax.getText() + ", " + inputMax2.getText()));
+    inputMax2.setOnKeyTyped(
+        e -> updateObserver("max input", inputMax.getText() + ", " + inputMax2.getText()));
 
     TextField inputMin = new TextField();
-    inputFieldStyle(inputMin,"ex. 0", 30, 80);
-    inputMin.setOnKeyTyped(e-> updateObserver("min input", inputMin.getText() + ", "
-        + inputMin.getText()));
+    inputFieldStyle(inputMin, "0", 30, 35);
 
+    TextField inputMin2 = new TextField();
+    inputFieldStyle(inputMin2, "0", 30, 35);
+
+    HBox minArea = new HBox(10);
+    minArea.getChildren().addAll(inputMin, inputMin2);
+
+    inputMin.setOnKeyTyped(
+        e -> updateObserver("min input", inputMin.getText() + ", " + inputMin2.getText()));
+    inputMin2.setOnKeyTyped(
+        e -> updateObserver("min input", inputMin.getText() + ", " + inputMin2.getText()));
 
     VBox minBox = new VBox(10);
     VBox maxBox = new VBox(10);
@@ -132,8 +163,8 @@ public class FactorialPage extends View {
     minMaxError.getStyleClass().add("error");
     updateErrorLabel("InputMinMax", minMaxError);
 
-    minBox.getChildren().addAll(minTitle, inputMin);
-    maxBox.getChildren().addAll(maxTitle, inputMax);
+    minBox.getChildren().addAll(minTitle, minArea);
+    maxBox.getChildren().addAll(maxTitle, maxArea);
 
     HBox minMaxBox = new HBox(20);
     minMaxBox.getChildren().addAll(minBox, maxBox);
@@ -149,28 +180,33 @@ public class FactorialPage extends View {
   private VBox gameButtons() {
 
     Button playButton = new Button("Play");
-    addStyle(playButton,"green", 180);
-    playButton.setOnAction(e-> updateObserver("button clicked", "play"));
+    playButton.getStyleClass().add("button2");
+    addStyle(playButton, "green", 180);
+    playButton.setOnAction(e -> updateObserver("button clicked", "play"));
 
-    Button resetButton = new Button("Reset");
-    addStyle(resetButton,"red",80);
-    resetButton.setOnAction(e-> updateObserver("button clicked", "reset"));
+    Button resetButton = new Button("Reset canvas");
+    resetButton.getStyleClass().add("button2");
+    addStyle(resetButton, "red", 180);
+    resetButton.setOnAction(e -> updateObserver("button clicked", "reset"));
 
     Button saveButton = new Button("Save to file");
-    addStyle(saveButton, "black", 80);
-    saveButton.setOnAction(e-> updateObserver("button clicked", "save"));
-
+    saveButton.getStyleClass().add("button2");
+    addStyle(saveButton, "black", 180);
+    saveButton.setOnAction(e -> updateObserver("button clicked", "save"));
 
     Button addTransformButton = new Button("Add transform");
-    addStyle(addTransformButton,"grey",180);
-    addTransformButton.setOnAction(e-> updateObserver("button clicked", "add"));
+    addTransformButton.getStyleClass().add("button2");
+    addStyle(addTransformButton, "grey", 180);
+    addTransformButton.setOnAction(e -> updateObserver("button clicked", "add"));
 
+    Button applyEditsButton = new Button("Edit transformation");
+    applyEditsButton.getStyleClass().add("button2");
+    addStyle(applyEditsButton, "blue", 180);
+    applyEditsButton.setOnAction(e -> updateObserver("button clicked", "edit"));
 
-    HBox topButtonBox = new HBox(20);
-    topButtonBox.getChildren().addAll(saveButton, resetButton);
-
-    VBox buttonBox = new VBox(30);
-    buttonBox.getChildren().addAll(addTransformButton, topButtonBox, playButton);
+    VBox buttonBox = new VBox(10);
+    buttonBox.getChildren()
+        .addAll(addTransformButton, applyEditsButton, saveButton, resetButton, playButton);
     buttonBox.setAlignment(Pos.CENTER);
 
     return buttonBox;
@@ -178,12 +214,13 @@ public class FactorialPage extends View {
 
   public void juliaSidebar() {
     createSideBar();
-    sidebarMenu.getChildren().addAll(chooseStepsField(),chooseMinMaxField(), createVector(),gameButtons());
+    sidebarMenu.getChildren()
+        .addAll(chooseStepsField(), chooseMinMaxField(), createVector(), gameButtons());
   }
 
   public void affineSidebar() {
     createSideBar();
-    sidebarMenu.getChildren().addAll(chooseStepsField(),chooseMinMaxField(), createVector(),
+    sidebarMenu.getChildren().addAll(chooseStepsField(), chooseMinMaxField(), createVector(),
         createMatrix(), gameButtons());
   }
 
@@ -192,22 +229,24 @@ public class FactorialPage extends View {
     chooseVector.getStyleClass().add("heading");
 
     TextField inputVectorX0 = new TextField();
-    inputFieldStyle(inputVectorX0,"x0", 30, 80);
+    inputFieldStyle(inputVectorX0, "x0", 30, 80);
     TextField inputVectorX1 = new TextField();
-    inputFieldStyle(inputVectorX1,"x1", 30, 80);
+    inputFieldStyle(inputVectorX1, "x1", 30, 80);
 
-    inputVectorX0.setOnKeyTyped(e-> updateObserver("vector input", inputVectorX0.getText() + ", " + inputVectorX1.getText()));
-    inputVectorX1.setOnKeyTyped(e-> updateObserver("vector input", inputVectorX0.getText() + ", " + inputVectorX1.getText()));
+    inputVectorX0.setOnKeyTyped(e -> updateObserver("vector input",
+        inputVectorX0.getText() + ", " + inputVectorX1.getText()));
+    inputVectorX1.setOnKeyTyped(e -> updateObserver("vector input",
+        inputVectorX0.getText() + ", " + inputVectorX1.getText()));
 
     HBox vectorInputs = new HBox(20);
     vectorInputs.getChildren().addAll(inputVectorX0, inputVectorX1);
 
     vectorErrorLabel = new Label();
     vectorErrorLabel.getStyleClass().add("error");
-    updateErrorLabel("InputVector",vectorErrorLabel);
+    updateErrorLabel("InputVector", vectorErrorLabel);
 
     VBox vectorArea = new VBox(10);
-    vectorArea.getChildren().addAll(chooseVector, vectorInputs,vectorErrorLabel);
+    vectorArea.getChildren().addAll(chooseVector, vectorInputs, vectorErrorLabel);
     vectorArea.setAlignment(Pos.CENTER);
     return vectorArea;
   }
@@ -217,112 +256,122 @@ public class FactorialPage extends View {
     createMatrix.getStyleClass().add("heading");
 
     TextField inputA = new TextField();
-    inputFieldStyle(inputA,"a", 30, 80);
+    inputFieldStyle(inputA, "a", 30, 80);
     TextField inputB = new TextField();
-    inputFieldStyle(inputB,"b", 30, 80);
+    inputFieldStyle(inputB, "b", 30, 80);
     TextField inputC = new TextField();
-    inputFieldStyle(inputC,"c", 30, 80);
+    inputFieldStyle(inputC, "c", 30, 80);
     TextField inputD = new TextField();
-    inputFieldStyle(inputD,"d", 30, 80);
+    inputFieldStyle(inputD, "d", 30, 80);
 
-    inputA.setOnKeyTyped(e-> updateObserver("matrix input", inputA.getText() + ", " + inputB.getText() + ", " + inputC.getText() + ", " + inputD.getText()));
-    inputB.setOnKeyTyped(e-> updateObserver("matrix input", inputA.getText() + ", " + inputB.getText() + ", " + inputC.getText() + ", " + inputD.getText()));
-    inputC.setOnKeyTyped(e-> updateObserver("matrix input", inputA.getText() + ", " + inputB.getText() + ", " + inputC.getText() + ", " + inputD.getText()));
-    inputD.setOnKeyTyped(e-> updateObserver("matrix input", inputA.getText() + ", " + inputB.getText() + ", " + inputC.getText() + ", " + inputD.getText()));
+    inputA.setOnKeyTyped(e -> updateObserver("matrix input",
+        inputA.getText() + ", " + inputB.getText() + ", " + inputC.getText() + ", "
+            + inputD.getText()));
+    inputB.setOnKeyTyped(e -> updateObserver("matrix input",
+        inputA.getText() + ", " + inputB.getText() + ", " + inputC.getText() + ", "
+            + inputD.getText()));
+    inputC.setOnKeyTyped(e -> updateObserver("matrix input",
+        inputA.getText() + ", " + inputB.getText() + ", " + inputC.getText() + ", "
+            + inputD.getText()));
+    inputD.setOnKeyTyped(e -> updateObserver("matrix input",
+        inputA.getText() + ", " + inputB.getText() + ", " + inputC.getText() + ", "
+            + inputD.getText()));
 
     matrixErrorLabel = new Label();
     matrixErrorLabel.getStyleClass().add("error");
     updateErrorLabel("InputMatrix", matrixErrorLabel);
-    HBox inputAB = new HBox(20, inputA,inputB);
+    HBox inputAB = new HBox(20, inputA, inputB);
     HBox inputCD = new HBox(20, inputC, inputD);
 
     VBox matrixArea = new VBox(10);
-    matrixArea.getChildren().addAll(createMatrix,inputAB,inputCD, matrixErrorLabel);
+    matrixArea.getChildren().addAll(createMatrix, inputAB, inputCD, matrixErrorLabel);
     matrixArea.setAlignment(Pos.CENTER);
 
     return matrixArea;
   }
 
-  private void handleSceneFChange(String info) {}
-  public void inputFieldStyle(TextField inputField, String promptText, int height,int width ) {
-    inputField.setMinHeight(height);
-    inputField.setMaxWidth(width);
-    inputField.setPromptText(promptText);
-  }
-
-  public void addStyle(Button button, String color, int width) {
-    String colorInit = "-fx-background-color: " + color + ";" ;
-    String widthInit = "-fx-min-width: " + width + ";" ;
-
-    button.setStyle(colorInit + widthInit);
-  }
-
-
-  @Override
-  public BorderPane getPane() {
-    return borderPane;
-  }
-  private void updateErrorLabel(String key, Label label) {
-    String errorMessage = errorMap.getOrDefault(key, "");
-    label.setText(errorMessage.isEmpty() ? " " : errorMessage);
-  }
-  public void showError(String placement, String message) {
-    errorMap.put(placement, message);
-    switch (placement) {
-      case "InputSteps":
-        updateErrorLabel("InputSteps", stepsErrorLabel);
-        break;
-      case "InputMinMax":
-        updateErrorLabel("InputMinMax", minMaxError);
-        break;
-      case "InputMatrix":
-        updateErrorLabel("InputMatrix", matrixErrorLabel);
-        break;
-      case "InputVector":
-        updateErrorLabel("InputVector", vectorErrorLabel);
-        break;
+    public void inputFieldStyle (TextField inputField, String promptText,int height, int width){
+      inputField.setMinHeight(height);
+      inputField.setMaxWidth(width);
+      inputField.setPromptText(promptText);
     }
-  }
 
-  public void draw(int steps) {
-    chaosGame.runSteps(steps);
 
-    int[][] canvas = chaosGame.getCanvas().getCanvasArray();
-    GraphicsContext gc = pixelCanvas.getGraphicsContext2D();
+    public void addStyle(Button button, String color,int width) {
+      String colorInit = "-fx-background-color: " + color + ";";
+      String widthInit = "-fx-min-width: " + width + ";";
 
-    for (int i = 0; i < canvas.length; i++) {
-      for (int j = 0; j < canvas[i].length; j++) {
-        double number = canvas[i][j] / 10f;
-        if ((number) > 0) {
-          if ((number) < 1) {
-            gc.setFill(Color.color(1, 0, number));
-          } else if ((number) < 2) {
-            gc.setFill(Color.color(2 - number, 0, 1));
-          } else {
-            gc.setFill(Color.color(0, 0, 1));
+      button.setStyle(colorInit + widthInit);
+    }
+
+    @Override
+    public BorderPane getPane() {
+      return borderPane;
+    }
+    private void updateErrorLabel(String key, Label label) {
+      String errorMessage = errorMap.getOrDefault(key, "");
+      label.setText(errorMessage.isEmpty() ? " " : errorMessage);
+    }
+    public void showError (String placement, String message) {
+      errorMap.put(placement, message);
+      switch (placement) {
+        case "InputSteps":
+          updateErrorLabel("InputSteps", stepsErrorLabel);
+          break;
+        case "InputMinMax":
+          updateErrorLabel("InputMinMax", minMaxError);
+          break;
+        case "InputMatrix":
+          updateErrorLabel("InputMatrix", matrixErrorLabel);
+          break;
+        case "InputVector":
+          updateErrorLabel("InputVector", vectorErrorLabel);
+          break;
+      }
+    }
+
+    public void draw( int steps){
+      chaosGame.runSteps(steps);
+
+      int[][] canvas = chaosGame.getCanvas().getCanvasArray();
+      GraphicsContext gc = pixelCanvas.getGraphicsContext2D();
+
+      for (int i = 0; i < canvas.length; i++) {
+        for (int j = 0; j < canvas[i].length; j++) {
+          double number = canvas[i][j] / 10f;
+          if ((number) > 0) {
+            if ((number) < 1) {
+              gc.setFill(Color.color(1, 0, number));
+            } else if ((number) < 2) {
+              gc.setFill(Color.color(2 - number, 0, 1));
+            } else {
+              gc.setFill(Color.color(0, 0, 1));
+            }
+            gc.fillRect(j, i, 1, 1); // Draw a single pixel
           }
-          gc.fillRect(j, i, 1, 1); // Draw a single pixel
         }
       }
     }
-  }
-  public void reset() {
-    GraphicsContext gc = pixelCanvas.getGraphicsContext2D();
-    gc.clearRect(0, 0, pixelCanvas.getWidth(), pixelCanvas.getHeight());
-  }
-  public ChaosGameDescription getDescription() {
-    return description;
-  }
-  public String getGameType() {
-    return gameType;
-  }
+    public void reset() {
+      GraphicsContext gc = pixelCanvas.getGraphicsContext2D();
+      gc.clearRect(0, 0, pixelCanvas.getWidth(), pixelCanvas.getHeight());
+    }
+    public ChaosGameDescription getDescription() {
+      return description;
+    }
+    public String getGameType() {
+      return gameType;
+    }
 
-  public void setGameType(String gameType) {
-    this.gameType = gameType;
-  }
-  public void setDescription(ChaosGameDescription description) {
-    this.description = description;
-    chaosGame = new ChaosGame(description, Math.round(MainLogic.width * 0.7f), Math.round(MainLogic.height * 0.7f));
+    public void setGameType (String gameType){
+      this.gameType = gameType;
+    }
+    public void setDescription (ChaosGameDescription description){
+      this.description = description;
+      chaosGame = new ChaosGame(description, Math.round(MainLogic.width * 0.7f),
+          Math.round(MainLogic.height * 0.7f));
 
-  }
+    }
 }
+
+
