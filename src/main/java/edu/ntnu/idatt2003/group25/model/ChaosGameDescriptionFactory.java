76fe67303604fb220c1d531dfe8488fd3adb9039
Johanne Fixdal <johanfix@stud.ntnu.no>
2024-05-19 11:20:37 +0200

    Fix: updated javaDoc for ChaosGameDescriptionFactory class

--- src/main/java/edu/ntnu/idatt2003/group25/model/ChaosGameDescriptionFactory.java
+++ src/main/java/edu/ntnu/idatt2003/group25/model/ChaosGameDescriptionFactory.java
@@ -12,6 +12,12 @@ import java.util.List;
  */
 public class ChaosGameDescriptionFactory {
 
+  /**
+   * Creates a ChaosGameDescription for the Sierpinski fractal.
+   *
+   * @return ChaosGameDescription object with values for the Sierpinski fractal.
+   */
+
   public static ChaosGameDescription createSierpinski() {
     List<Transform2D> transformations = new ArrayList<>();
     transformations.add(new AffineTransform2D(new Matrix2x2(0.5, 0, 0, 0.5),
@@ -23,6 +29,11 @@ public class ChaosGameDescriptionFactory {
     return new ChaosGameDescription(transformations, new Vector2D(0, 0), new Vector2D(1, 1));
   }
 
+  /**
+   * Creates a ChaosGameDescription for the Barnsley Fern fractal.
+   *
+   * @return ChaosGameDescription object with values for the Barnsley Fern fractal.
+   */
   public static ChaosGameDescription createBarnsleyFern() {
     List<Transform2D> transformations = new ArrayList<>();
     transformations.add(new AffineTransform2D(new Matrix2x2(0, 0, 0, 0.16),
@@ -37,12 +48,25 @@ public class ChaosGameDescriptionFactory {
         new Vector2D(-2.64, 0), new Vector2D(2.65, 10));
   }
 
+  /**
+   * Creates a ChaosGameDescription for the Julia fractal.
+   *
+   * @param c Complex number used in the Julia fractal.
+   * @return ChaosGameDescription object with values for the Julia fractal.
+   */
+
   public static ChaosGameDescription createJuliaTransformation(Complex c) {
     List<Transform2D> transformation = new ArrayList<>();
     transformation.add(new JuliaTransform(c, 1));
-    transformation.add(new JuliaTransform(c,-1));
+    transformation.add(new JuliaTransform(c, -1));
     return new ChaosGameDescription(transformation, new Vector2D(-1.6, -1), new Vector2D(1.6, 1));
   }
+
+  /**
+   * Creates a ChaosGameDescription for the Levy Dragon fractal.
+   *
+   * @return ChaosGameDescription object with values for the Levy Dragon fractal.
+   */
   public static ChaosGameDescription createLevyDragon() {
     List<Transform2D> transformations = new ArrayList<>();
     transformations.add(new AffineTransform2D(new Matrix2x2(0.5, -0.5, 0.5, 0.5),
@@ -53,6 +77,12 @@ public class ChaosGameDescriptionFactory {
     return new ChaosGameDescription(transformations,
         new Vector2D(-0.5, -0.5), new Vector2D(1.5, 1));
   }
+
+  /**
+   * Creates a ChaosGameDescription for the Heighway Dragon fractal.
+   *
+   * @return ChaosGameDescription object with values for the Heighway Dragon fractal.
+   */
   public static ChaosGameDescription createHeighwayDragon() {
     List<Transform2D> transformations = new ArrayList<>();
     transformations.add(new AffineTransform2D(new Matrix2x2(0.5, -0.5, 0.5, 0.5),
@@ -63,9 +93,14 @@ public class ChaosGameDescriptionFactory {
     return new ChaosGameDescription(transformations,
         new Vector2D(-0.5, -0.5), new Vector2D(1.25, 0.75));
   }
-//  Add Koch snowflake?
+
+  /**
+   * Creates an empty ChaosGameDescription.
+   *
+   * @return ChaosGameDescription object with empty values.
+   */
   public static ChaosGameDescription createEmpty() {
     return new ChaosGameDescription(new ArrayList<>(),
-        new Vector2D(0, 0), new Vector2D(0,0));
+        new Vector2D(0, 0), new Vector2D(0, 0));
   }
 }
