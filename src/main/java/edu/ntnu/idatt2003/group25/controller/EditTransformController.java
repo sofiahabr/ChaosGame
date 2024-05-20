5bc66e36f7740a0b629ed56d61fafa6198ff91ed
Sofia Håbrekke <sofiash@stud.ntnu.no>
2024-05-20 19:39:29 +0200

    Feat: Add method for handling sign for julia transforms

--- src/main/java/edu/ntnu/idatt2003/group25/controller/EditTransformController.java
+++ src/main/java/edu/ntnu/idatt2003/group25/controller/EditTransformController.java
@@ -15,12 +15,14 @@ import java.util.List;
 
 public class EditTransformController extends Controller{
   String invalidNumber = " must be numbers";
+  String oneOrNegOne = " must be 1 or negative -1";
 
   int defaultValue = -9999;
   EditTransformsMenu editTransformsMenu;
   FactorialPage factorialPage;
     private List<Vector2D> vectorsInDescription = new ArrayList<>();
     private List<Matrix2x2> matrixInDescription = new ArrayList<>();
+    private List<Integer> signInput = new ArrayList<>();
 
     public EditTransformController(EditTransformsMenu editTransformsMenu, FactorialPage factorialPage) {
         this.editTransformsMenu = editTransformsMenu;
@@ -32,8 +34,10 @@ public class EditTransformController extends Controller{
     switch (event) {
       case "save vectors" -> saveVectors(info);
       case "save matrix" -> saveMatrix(info);
+      case "save signs" -> saveSigns(info);
       case "edit description" -> editDescription(info);
       case "min input", "max input" -> handleInputVector("InputMinMax", info, "Min and max");
+      case "sign input" -> handleIntInput("InputSign", info, "Sign");
       case "matrix input" -> handleInputMatrix("InputMatrix", info, "Matrix's");
       case "vector input" -> handleInputVector("InputVector", info, "Vectors");
     }
@@ -63,24 +67,41 @@ public class EditTransformController extends Controller{
       }
       this.matrixInDescription = matrixList;
     }
+  private void saveSigns(String info) {
+    System.out.println("Signs: " + info);
+    String[] signNumbers = info.split(",");
+
+    List<Integer> signList = new ArrayList<>();
+
+    for (String signNumber : signNumbers) {
+      if (!signNumber.trim().isEmpty()) {
+        int sign = Validation.verifyInt(signNumber, -3);
+        signList.add(sign);
+      }
+    }
+    this.signInput = signList;
+  }
     private void editDescription(String info) {
       System.out.println("Min and max: " + info);
       String[] minMax = info.split(",");
 
       List<Transform2D> transforms = new ArrayList<>();
+      ChaosGameDescription description = new ChaosGameDescription(new ArrayList<>(), new Vector2D(Validation.verifyDouble(minMax[0], 0), Validation.verifyDouble(minMax[1], 0)),
+          new Vector2D(Validation.verifyDouble(minMax[2], 0), Validation.verifyDouble(minMax[3], 0)));
+
       if(factorialPage.getGameType().equals("Affine Transform") && matrixInDescription.size() == vectorsInDescription.size()) {
         for(int i = 0; i < matrixInDescription.size(); i++) {
           transforms.add(new AffineTransform2D(matrixInDescription.get(i), vectorsInDescription.get(i)));
         }
       }
-      if(factorialPage.getGameType().equals("Julia Transform")) {
-        for(int i = 0; i < vectorsInDescription.size(); i++) {
-          transforms.add(new JuliaTransform(new Complex(vectorsInDescription.get(i).getX0(), vectorsInDescription.get(i).getX1()),1));
+      if(factorialPage.getGameType().equals("Julia Transform") && vectorsInDescription.size() == signInput.size()) {
+        for (int i = 0; i < vectorsInDescription.size(); i++) {
+          System.out.println("Sign " + i + ": " + signInput.get(i));
+          transforms.add(new JuliaTransform(new Complex(vectorsInDescription.get(i).getX0(), vectorsInDescription.get(i).getX1()), signInput.get(i)));
         }
       }
-      factorialPage.setDescription(new ChaosGameDescription(transforms,
-          new Vector2D(Validation.verifyDouble(minMax[0], 0), Validation.verifyDouble(minMax[1], 0)),
-          new Vector2D(Validation.verifyDouble(minMax[2], 0), Validation.verifyDouble(minMax[3], 0))));
+      description.setTransforms(transforms);
+      factorialPage.setDescription(description);
     }
   public void handleInputVector(String key, String info, String name){
     System.out.println(info);
@@ -91,9 +112,17 @@ public class EditTransformController extends Controller{
         Validation.verifyDouble(values[1], defaultValue));
 
     if (vector.getX0() == defaultValue || vector.getX1() == defaultValue) {
-      editTransformsMenu.showError(key, name + invalidNumber);
+      editTransformsMenu.showError(key, name + oneOrNegOne);
     } else {
       editTransformsMenu.showError(key, "");
+    }
+  }
+  private void handleIntInput(String key, String info, String name) {
+    int sign = Validation.verifyInt(info, -2);
+    if (sign == 1 || sign == -1) {
+      editTransformsMenu.showError(key, "");
+    } else {
+      editTransformsMenu.showError(key, name + oneOrNegOne);
     }
   }
   public void handleInputMatrix(String key, String info, String name){
