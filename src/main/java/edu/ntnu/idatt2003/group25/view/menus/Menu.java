e5703ba6d694e64d13d82b9256db0807693a1131
Sofia Håbrekke <sofiash@stud.ntnu.no>
2024-05-20 19:59:04 +0200

    Refactor: Delete unnecessary imports

--- src/main/java/edu/ntnu/idatt2003/group25/view/menus/Menu.java
+++ src/main/java/edu/ntnu/idatt2003/group25/view/menus/Menu.java
@@ -1,29 +1,17 @@
 package edu.ntnu.idatt2003.group25.view.menus;
 
 import edu.ntnu.idatt2003.group25.controller.Controller;
-import edu.ntnu.idatt2003.group25.controller.FactorialPageController;
-import edu.ntnu.idatt2003.group25.controller.ScreenController;
 import edu.ntnu.idatt2003.group25.model.ChaosGameObserver;
 import edu.ntnu.idatt2003.group25.model.ChaosGameSubject;
-import edu.ntnu.idatt2003.group25.view.FactorialPage;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Node;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
 
 public abstract class Menu implements ChaosGameSubject {
-  private List<ChaosGameObserver> observers = new ArrayList<>();
+  private final List<ChaosGameObserver> observers = new ArrayList<>();
 
-  public Menu(ScreenController screenController) {
-    addObserver(screenController);
+  public Menu(Controller controller) {
+    addObserver(controller);
   }
 
   @Override
