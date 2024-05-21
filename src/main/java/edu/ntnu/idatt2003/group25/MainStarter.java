38841abb574822282b8bd85839df0324d2c81821
Sofia Håbrekke <sofiash@stud.ntnu.no>
2024-05-21 21:04:59 +0200

    Docs: Add javadoc for the MainStarter class

---
+++ src/main/java/edu/ntnu/idatt2003/group25/MainStarter.java
@@ -0,0 +1,12 @@
+package edu.ntnu.idatt2003.group25;
+
+public class MainStarter {
+  /**
+   * The main method that starts the application.
+   *
+   * @param args the arguments
+   */
+  public static void main(String[] args) {
+    MainView.main(args);
+  }
+}
--- src/main/java/edu/ntnu/idatt2003/group25/view/MainStarter.java
+++
@@ -1,7 +0,0 @@
-package edu.ntnu.idatt2003.group25.view;
-
-public class MainStarter {
-  public static void main(String[] args) {
-    MainView.main(args);
-  }
-}
