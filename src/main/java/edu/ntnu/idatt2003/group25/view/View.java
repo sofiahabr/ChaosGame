e64b9633b1af168018d4d7d9fd3d1934e7f4d104
Sofia Håbrekke <sofiash@stud.ntnu.no>
2024-05-21 21:06:15 +0200

    Docs: Added javadocs for View class

--- src/main/java/edu/ntnu/idatt2003/group25/view/View.java
+++ src/main/java/edu/ntnu/idatt2003/group25/view/View.java
@@ -5,27 +5,59 @@ import edu.ntnu.idatt2003.group25.model.ChaosGameSubject;
 import java.util.ArrayList;
 import java.util.List;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
 
+/**
+ * The type View, that represents a page to display on the screen.
+ */
 public abstract class View implements ChaosGameSubject {
+  /**
+   * The Observers, a list of observers that observes the view.
+   */
   private List<ChaosGameObserver> observers = new ArrayList<>();
+
+  /**
+   * The method that adds an observer to the view.
+   * @param observer the observer that is to be added.
+   */
   @Override
   public void addObserver(ChaosGameObserver observer) {
     observers.add(observer);
   }
+
+  /**
+   * The method that removes an observer from the view.
+   *
+   * @param observer the observer that is to be removed.
+   */
 
   @Override
   public void removeObserver(ChaosGameObserver observer) {
     observers.remove(observer);
   }
 
+  /**
+   * The method that updates the observers of the view.
+   *
+   * @param event the event that has happened
+   * @param info the information that is to be sent to the observers
+   */
+
   @Override
   public void updateObserver(String event, String info) {
-    for (ChaosGameObserver observer : observers){
+    for (ChaosGameObserver observer : observers) {
       observer.gameChanged(event, info);
     }
   }
 
+  /**
+   * A method for accessing panes of the views.
+   *
+   * @return the pane
+   */
   public abstract BorderPane getPane();
+
+  /**
+   * The method for setting up the UI- layout.
+   */
   public abstract void setUp();
 }
