7b6c25b49200852aa49d785607f6437c101a1ac8
Sofia Håbrekke <sofiash@stud.ntnu.no>
2024-05-21 21:01:38 +0200

    Docs: Add javadoc for the Controller abstract class

--- src/main/java/edu/ntnu/idatt2003/group25/controller/Controller.java
+++ src/main/java/edu/ntnu/idatt2003/group25/controller/Controller.java
@@ -2,6 +2,9 @@ package edu.ntnu.idatt2003.group25.controller;
 
 import edu.ntnu.idatt2003.group25.model.ChaosGameObserver;
 
+/**
+ * The type Controller that implements Observer.
+ */
 public abstract class Controller implements ChaosGameObserver {
 
 }
