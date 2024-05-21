735dd67b5f9400bc7331713cad24795ccbdd21e8
Sofia Håbrekke <sofiash@stud.ntnu.no>
2024-05-21 21:02:31 +0200

    Docs: Add javadoc for FactorialPageController

--- src/main/java/edu/ntnu/idatt2003/group25/controller/FactorialPageController.java
+++ src/main/java/edu/ntnu/idatt2003/group25/controller/FactorialPageController.java
@@ -2,7 +2,6 @@ package edu.ntnu.idatt2003.group25.controller;
 
 import edu.ntnu.idatt2003.group25.model.ChaosGame;
 import edu.ntnu.idatt2003.group25.model.ChaosGameDescription;
-import edu.ntnu.idatt2003.group25.model.ChaosGameDescriptionFactory;
 import edu.ntnu.idatt2003.group25.model.ChaosGameFileHandler;
 import edu.ntnu.idatt2003.group25.model.Complex;
 import edu.ntnu.idatt2003.group25.model.Matrix2x2;
@@ -10,128 +9,163 @@ import edu.ntnu.idatt2003.group25.model.Validation;
 import edu.ntnu.idatt2003.group25.model.Vector2D;
 import edu.ntnu.idatt2003.group25.model.transforms.AffineTransform2D;
 import edu.ntnu.idatt2003.group25.model.transforms.JuliaTransform;
-import edu.ntnu.idatt2003.group25.view.MainLogic;
 import edu.ntnu.idatt2003.group25.view.FactorialPage;
+import edu.ntnu.idatt2003.group25.view.MainLogic;
 import edu.ntnu.idatt2003.group25.view.menus.EditTransformsMenu;
 import java.io.File;
 import java.util.ArrayList;
-import javafx.scene.canvas.Canvas;
 import javafx.stage.FileChooser;
 
 /**
- * FactorialPageController function as the FactorialPage's controller class.
+ * FactorialPageController functions as the FactorialPage's controller class.
  */
 public class FactorialPageController extends Controller {
+  /**
+   * The Screen controller, the controller that controls what view is displayed on the screen.
+   */
   ScreenController screenController;
+  /**
+   * The Factorial page, the view page where the game is displayed.
+   */
   FactorialPage factorialPage;
+  /**
+   * The steps the game will run.
+   */
   int steps = 0;
-  Vector2D min = new Vector2D(0,0);
-  Vector2D max = new Vector2D(0,0);
-  Vector2D vector2D = new Vector2D(0,0);
-  Complex complex = new Complex(0,0);
-  Matrix2x2 matrix = new Matrix2x2(0,0,0,0);
+  /**
+   * The Min coordinates for a transformation.
+   */
+  Vector2D min = new Vector2D(0, 0);
+  /**
+   * The Max coordinates for a transformation.
+   */
+  Vector2D max = new Vector2D(0, 0);
+  /**
+   * The Vector2D for the transformation.
+   */
+  Vector2D vector2D = new Vector2D(0, 0);
+  /**
+   * The Complex, the complex vector for the transformation.
+   */
+  Complex complex = new Complex(0, 0);
+  /**
+   * The Matrix the transformation will use.
+   */
+  Matrix2x2 matrix = new Matrix2x2(0, 0, 0, 0);
+  /**
+   * The sign for the transformation.
+   */
   int sign = 0;
+  /**
+   * The error message displayed when the input in not a number or a negative number.
+   */
   String invalidPositiveNumber = "Please enter a positive \nnumber 0 -1000 000 000";
+  /**
+   * The error message displayed when the input in not a number.
+   */
   String invalidNumber = "Please enter numbers";
+  /**
+   * The error message displayed when the input in not 1 or -1.
+   */
   String oneOrNegOne = "Number must be 1 or -1";
+  /**
+   * The default value for validation and error handling.
+   */
   int defaultValue = -9999;
 
   /**
    * The FactorialPageController takes in instances of ScreenController and FactorialPage.
-   * @param factorialPage
+   * The FactorialPageController is responsible for handling the user input and updating the FactorialPage.
+   *
+   * @param screenController the screen controller
+   * @param factorialPage    the factorial page
    */
-
   public FactorialPageController(ScreenController screenController, FactorialPage factorialPage) {
     this.screenController = screenController;
     this.factorialPage = factorialPage;
 
   }
 
+  /**
+   * The method for handling the user input and handling the game accordingly.
+   * The method is called when the user interacts with the view.
+   *
+   * @param event the action / event the user has done
+   * @param info the information linked to the event
+   */
+
   @Override
-  public void gameChanged(String action, String info) {
-    switch (action) {
+  public void gameChanged(String event, String info) {
+    switch (event) {
       case "button clicked" -> buttonClicked(info);
-      case "vector input" -> vector2D = handleInputVector(info, "InputVector");
-      case "min input" -> min = handleInputVector(info, "InputMinMax");
-      case "max input" -> max = handleInputVector(info, "InputMinMax");
-      case "sign input" -> sign = handleInputSign(info);
       case "register steps" -> steps = handleInputSteps(info);
-      case "matrix input" -> matrix = handleInputMatrix(info);
     }
   }
-  public void buttonClicked(String info){
+
+  /**
+   * The method for handling the users interaction when it has clicked on a button.
+   *
+   * @param info the button clicked
+   */
+  public void buttonClicked(String info) {
     switch (info) {
-      case "play"-> {
+      case "play" -> {
         factorialPage.getChaosGame().runSteps(steps);
         factorialPage.draw();
       }
-      case "add"-> addAction();
-      case "reset"-> resetAction();
+      case "add" -> addAction();
+      case "reset" -> resetAction();
       case "save" -> saveAction();
       case "edit" -> new EditTransformsMenu(screenController, factorialPage);
     }
   }
-    public int handleInputSign(String input) {
-      int sign = Validation.verifyInt(input, 0);
-      if (sign == 1 || sign == -1) {
-        factorialPage.showError("InputSign", "");
-        return sign;
-      } else {
-        factorialPage.showError("InputSign", oneOrNegOne);
-      }
-      return 0; //Default value
-    }
+
+  /**
+   * The method for handling the user input in steps text field, and validating the input.
+   *
+   * @param input the input received from the view.
+   * @return the amount of steps the user wants to run, or 0 if the input is invalid.
+   */
   public int handleInputSteps(String input) {
-    int inputSteps = Validation.verifyPositiveInt(input,0);
+    int inputSteps = Validation.verifyPositiveInt(input, 0);
     if (inputSteps == 0) {
       factorialPage.showError("InputSteps", invalidPositiveNumber);
-    }
-    else {
+    } else {
       factorialPage.showError("InputSteps", "");
     }
     return inputSteps;
   }
-  public Vector2D handleInputVector(String info, String key){
-    String[] values = info.split(",");
-    Vector2D vector = new Vector2D(Validation.verifyDouble(values[0], defaultValue),
-        Validation.verifyDouble(values[1], defaultValue));
 
-    if (vector.getX0() == defaultValue || vector.getX1() == defaultValue) {
-      factorialPage.showError(key, invalidNumber);
-    } else {
-      factorialPage.showError(key, " ");
-      return vector;
-    }
-    return null;
-  }
-  public Matrix2x2 handleInputMatrix(String info){
-    String[] values = info.split(",");
-    Matrix2x2 matrix2x2 = new Matrix2x2(
-        Validation.verifyDouble(values[0],defaultValue), Validation.verifyDouble(values[1],defaultValue),
-        Validation.verifyDouble(values[2],defaultValue), Validation.verifyDouble(values[3],defaultValue));
+  /**
+   * The method for adding a transformation to the game's description.
+   * The method is called when the user clicks the add button.
+   * The method adds an empty transformation to the game's description.
+   */
 
-    if (matrix2x2.getA00() == defaultValue || matrix2x2.getA01() == defaultValue ||
-        matrix2x2.getA10() == defaultValue || matrix2x2.getA11() == defaultValue) {
-      factorialPage.showError("InputMatrix", invalidNumber);
-    } else {
-      factorialPage.showError("InputMatrix", "");
-      return matrix2x2;
+  private void addAction() {
+    if (factorialPage.getGameType().equals("Julia Transform")) {
+      factorialPage.getDescription().getTransforms().add(new JuliaTransform(complex, sign));
+      factorialPage.setUp();
+    } else if (factorialPage.getGameType().equals("Affine Transform")) {
+      factorialPage.getDescription().getTransforms().add(new AffineTransform2D(matrix, vector2D));
+      factorialPage.setUp();
     }
-    return null;
   }
-private void addAction() {
-  if (factorialPage.getGameType().equals("Julia Transform")) {
-    factorialPage.getDescription().getTransforms().add(new JuliaTransform(complex, sign));
-    factorialPage.setUp();
-  } else if (factorialPage.getGameType().equals("Affine Transform")) {
-    factorialPage.getDescription().getTransforms().add(new AffineTransform2D(matrix, vector2D));
-    factorialPage.setUp();
-  }
-}
+
+  /**
+   * The method for resetting the canvas where the factorials are drawn.
+   * It also resets the canvas' registered pixels.
+   */
+
   private void resetAction() {
     factorialPage.reset();
     factorialPage.getChaosGame().getCanvas().clear();
   }
+
+  /**
+   * The method for saving the game's description to a file.
+   */
+
   private void saveAction() {
     ChaosGameFileHandler fileHandler =
         new ChaosGameFileHandler(new ArrayList<>(), new Vector2D(0, 0),
@@ -149,11 +183,12 @@ private void addAction() {
       }
     }
   }
+
+  /**
+   * The method for handling the screen resize event.
+   */
   public void handleScreenResize(double newWidth, double newHeight) {
-    int canvasWidth = (int) (newWidth * 0.7);
-    int canvasHeight = (int) (newHeight * 0.7);
     ChaosGameDescription description = factorialPage.getDescription();
-    ChaosGame newChaosGame = new ChaosGame(description, canvasWidth, canvasHeight);
     factorialPage.setDescription(description);
 
     factorialPage.reset();
