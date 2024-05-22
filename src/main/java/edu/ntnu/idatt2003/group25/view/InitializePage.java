a492766e0335d55818fabe038c4390812372177f
Johanne Fixdal <johanfix@stud.ntnu.no>
2024-05-22 10:49:10 +0200

    Style: fix style

--- src/main/java/edu/ntnu/idatt2003/group25/view/InitializePage.java
+++ src/main/java/edu/ntnu/idatt2003/group25/view/InitializePage.java
@@ -4,7 +4,6 @@ package edu.ntnu.idatt2003.group25.view;
 import edu.ntnu.idatt2003.group25.controller.InitializePageController;
 import edu.ntnu.idatt2003.group25.controller.ScreenController;
 import edu.ntnu.idatt2003.group25.view.menus.TopMenu;
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
@@ -31,7 +30,7 @@ public class InitializePage extends View {
    *
    * @param screenController the screen controller
    */
-  public InitializePage(ScreenController screenController){
+  public InitializePage(ScreenController screenController) {
     this.screenController = screenController;
     addObserver(new InitializePageController(screenController));
     setUp();
@@ -47,27 +46,27 @@ public class InitializePage extends View {
     topMenu.setUp();
     initPane.setTop(topMenu.getMenu());
 
-    StackPane infoBoxBackground = new StackPane();
-    Rectangle box = new Rectangle(MainLogic.width*0.6, MainLogic.height*0.6);
+
+    Rectangle box = new Rectangle(MainLogic.width * 0.6, MainLogic.height * 0.6);
     box.getStyleClass().add("rectangle");
 
     Text heading = new Text("Chaos Game");
     heading.getStyleClass().add("heading");
     heading.setStyle("-fx-font-size: 40");
 
-        Text subheading = new Text("Choose an option:");
+    Text subheading = new Text("Choose an option:");
     subheading.getStyleClass().add("heading");
     subheading.setStyle("-fx-font-size: 20");
 
     ComboBox<String> optionBox = new ComboBox<>();
     optionBox.getItems().addAll(
-        "Create new Affine Transform","Create new Julia Transform", "Julia Transform",
-        "Sierpinski Triangle", "Barnsley Fern","Lévy Dragon", "Heighway Dragon", "Read from file");
+        "Create new Affine Transform", "Create new Julia Transform", "Julia Transform",
+        "Sierpinski Triangle", "Barnsley Fern", "Lévy Dragon", "Heighway Dragon", "Read from file");
 
     optionBox.setMinSize(180, 30);
 
     Button startButton  = new Button("Start Game");
-    startButton.setOnAction(e-> {
+    startButton.setOnAction(e -> {
       if (optionBox.getValue() == null) {
         optionBox.setPromptText("Please choose a transform");
         optionBox.getStyleClass().add("error");
@@ -79,6 +78,7 @@ public class InitializePage extends View {
     VBox infoArea = new VBox(30, heading, subheading, optionBox, new VBox(), startButton);
     infoArea.setAlignment(Pos.CENTER);
 
+    StackPane infoBoxBackground = new StackPane();
     infoBoxBackground.getChildren().addAll(box, infoArea);
     infoBoxBackground.setAlignment(Pos.CENTER);
 
