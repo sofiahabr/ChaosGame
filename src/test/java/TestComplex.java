a000986bade60acb5fcf60f3a1b078ff2f2a931c
Johanne Fixdal <johanfix@stud.ntnu.no>
2024-05-22 11:47:21 +0200

    Test: improved TestComplex with more tests

--- src/test/java/TestComplex.java
+++ src/test/java/TestComplex.java
@@ -24,5 +24,25 @@ public class TestComplex {
       assertEquals(1.5537739740300374, test.sqrt().getX0());
       assertEquals(0.6435942529055827, test.sqrt().getX1());
     }
+
+    /**
+     * Test the sqrt method returns correct for both X0 and X1 for negative complex values.
+     */
+    @Test
+    void testSqrtNegativeValues() {
+      Complex negativeTest = new Complex(-2,-2);
+      assertEquals(0.6435942529055827, negativeTest.sqrt().getX0(),1e-9);
+      assertEquals(-1.5537739740300374, negativeTest.sqrt().getX1(),1e-9);
+    }
+
+    /**
+     * Test the sqrt method returns correct for both X0 and X1 for zero.
+     */
+    @Test
+    void testSqrtZero() {
+      Complex zeroTest = new Complex(0,0);
+      assertEquals(0, zeroTest.sqrt().getX0());
+      assertEquals(0, zeroTest.sqrt().getX1());
+    }
   }
 }
