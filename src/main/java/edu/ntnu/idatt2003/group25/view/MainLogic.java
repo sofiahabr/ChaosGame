6a1109da556d64db2b141fa1f1a3753c3eaabf1b
Johanne Fixdal <johanfix@stud.ntnu.no>
2024-05-22 10:49:29 +0200

    Style: fix style

--- src/main/java/edu/ntnu/idatt2003/group25/view/MainLogic.java
+++ src/main/java/edu/ntnu/idatt2003/group25/view/MainLogic.java
@@ -2,8 +2,6 @@ package edu.ntnu.idatt2003.group25.view;
 
 import edu.ntnu.idatt2003.group25.controller.FactorialPageController;
 import edu.ntnu.idatt2003.group25.controller.ScreenController;
-import edu.ntnu.idatt2003.group25.model.ChaosGameDescription;
-import edu.ntnu.idatt2003.group25.model.ChaosGameDescriptionFactory;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
@@ -13,31 +11,31 @@ import javafx.stage.Stage;
 /**
  * The type Main logic that holds the values and attributes necessary to run the project.
  */
-public class MainLogic extends View{
+public class MainLogic extends View {
   /**
    * The constant height of the application, based on the individual screen size.
    */
-  public static int height = (int) Math.round(Screen.getPrimary().getBounds().getHeight()*0.85f);
+  public static int height = (int) Math.round(Screen.getPrimary().getBounds().getHeight() * 0.85f);
   /**
    * The constant width of the application, based on the individual screen size.
    */
-  public static int width = (int) Math.round(Screen.getPrimary().getBounds().getWidth()*0.8f);
+  public static int width = (int) Math.round(Screen.getPrimary().getBounds().getWidth() * 0.8f);
   /**
    * The Scene the view is displayed on.
    */
-  public Scene scene = new Scene(new Pane(), width, height);
+  private final Scene scene = new Scene(new Pane(), width, height);
   /**
    * The Screen controller, the object that controls what screen is showed.
    */
-  public ScreenController screenController = new ScreenController(scene);
+  private final ScreenController screenController = new ScreenController(scene);
   /**
    * The Initialize page the view representing the .
    */
-  public InitializePage initializePage = new InitializePage(screenController);
+  private final InitializePage initializePage = new InitializePage(screenController);
   /**
    * The Factorial page.
    */
-  public FactorialPage factorialPage = new FactorialPage(screenController);
+  private final FactorialPage factorialPage = new FactorialPage(screenController);
 
   /**
    * Start.
@@ -47,35 +45,45 @@ public class MainLogic extends View{
   public void start(Stage stage) {
     screenController.addScreenContent(initializePage, "initialize page");
 
-    screenController.gameChanged( "switch page","initialize page");
+    screenController.gameChanged("switch page", "initialize page");
     scene.getStylesheets().add("/style/style.css");
 
     stage.setTitle("Chaos Game");
     stage.setScene(scene);
-    stage.setMinHeight(height*0.5);
-    stage.setMinWidth(width*0.5);
+    stage.setMinHeight(height * 0.5);
+    stage.setMinWidth(width * 0.5);
     stage.show();
 
-    FactorialPageController factorialPageController = new FactorialPageController(screenController, factorialPage);
+    FactorialPageController factorialPageController =
+        new FactorialPageController(screenController, factorialPage);
 
     addObserver(factorialPageController);
 
-    scene.widthProperty().addListener((observable, oldValue, newValue)-> {
+    scene.widthProperty().addListener((observable, oldValue, newValue) -> {
       updateObserver("sceneChange", "0:" + newValue.toString());
-      MainLogic.width = newValue.intValue();
+      width = newValue.intValue();
     });
 
-    scene.heightProperty().addListener((observable, oldValue, newValue)-> {
+    scene.heightProperty().addListener((observable, oldValue, newValue) -> {
       updateObserver("sceneChange", newValue.toString() + ":0");
-      MainLogic.height = newValue.intValue();
+      height = newValue.intValue();
     });
   }
+
+  /**
+   * Gets pane.
+   *
+   * @return the pane
+   */
 
   @Override
   public BorderPane getPane() {
     return null;
   }
 
+  /**
+   * The setUp method is extended by view, but doesn't have any returns in this class.
+   */
   @Override
   public void setUp() {
   }
